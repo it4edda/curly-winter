0dead279cc2cdf9d76a33d2356693c1ed8a9a0f7
Nils Sörell <49202692+NilsSir-l@users.noreply.github.com>
2025-06-26 19:13:15 +0200

    movement ⏰

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] float topSpeed;
     [SerializeField] Rigidbody rb;
     [SerializeField] Animator anim;
-    void Update()
+    Vector2 moveVal;
+    Vector3 forceToApply;
+
+    void Start()
     {
-        rb.AddForce(Vector3.right * speed);
+        rb.maxLinearVelocity = topSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(forceToApply * speed, ForceMode.Impulse);
+    }
+
+    public void Move(InputAction.CallbackContext InputContext)
+    {
+        moveVal = InputContext.ReadValue<Vector2>();
+       
+        forceToApply = new Vector3(moveVal.x, 0, moveVal.y);
     }
 }
