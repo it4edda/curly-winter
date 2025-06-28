db69584496ae5a77190317ea19238b05fc7440a2
William Edmalm <nilswilliam2005@gmail.com>
2025-06-29 01:04:17 +0200

    experimental player selection lol

--- Assets/Scripts/Identity/PlayerIdentity.cs
+++ Assets/Scripts/Identity/PlayerIdentity.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerIdentity", menuName = "Scriptable Objects/PlayerIdentity")]
 public class PlayerIdentity : ScriptableObject
 {
+    public bool hasSelected = false;
     public Color color;
     public ChosenClass chosenClass; 
 }
 public enum ChosenClass
 {
-    Magician, Ranger, Monk, Rogue, Priest, Summoner
+    Magician, Priest, Tank, Swordsman
+    //summoner? Ranger? subclasses ????
 }
 //GIVE PLAYER ITS OWN COLOR AND LET THEM CHOSE CLASS
